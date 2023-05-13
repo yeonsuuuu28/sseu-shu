51551a0530e09562e6733e8c75cc47fe56bd2789
yeonsuuuu28 <yeonsuuuu28@gmail.com>
2023-05-14 00:26:03 +0900

    add google authentification

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,14 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { Button, TextField }  from '@mui/material';
-import { db, auth, analytics } from "../firebase.js";
+import { db, auth, analytics, signInWithGoogle, signOutWithGoogle } from "../firebase.js";
+import { getAuth, getRedirectResult, GoogleAuthProvider } from "firebase/auth";
 import { ref, set, update, child, get, onValue, getDatabase } from "firebase/database";
 import Logo from "../Assets/logo_3.png"
 
 const Main = () => {
+    const [userId, setUserId] = useState("SIGN IN")
+    const [userInfo, setUserInfo] = useState(null)
+    let test = ""
+
+    useEffect(() => {
+        getRedirectResult(auth).then((result) => {
+            const user = result.user;
+            console.log(user)
+            setUserInfo(user)
+        })
+      }, []);
+
+
     return (
         <div style={{fontFamily: "Pretendard", textAlign: "left"}}>
-            <div style={{backgroundColor: "#2B04BE", height: "10vh"}}>
+            <div style={{backgroundColor: "#2B04BE", height: "10vh", lineHeight: "10vh"}}>
+                {userInfo === null ? 
+                    <div style={{float: "right", color: "white", paddingRight: "2vw", fontWeight: "700", verticalAlign: "middle", fontSize: "13pt", cursor: "pointer"}}
+                        onClick={signInWithGoogle} href="# ">
+                        SIGN IN
+                    </div>
+                    :
+                    <div style={{float: "right", color: "white", paddingRight: "2vw", fontWeight: "700", verticalAlign: "middle", fontSize: "13pt", cursor: "pointer"}}
+                        onClick={() => {signOutWithGoogle(); setUserInfo(null); window.location.reload();}} href="# ">
+                        {userInfo.displayName}
+                    </div>
+                }
+
                 <div style={{fontSize: "17pt", fontWeight: "500", color: "white", 
                     lineHeight: "3.0vh", height: "3.0vh", letterSpacing: "0.75px", 
                     paddingTop: "1vh", paddingLeft: "1vw"}}>
