5626925d349a21e45f195d437c381588f03db301
yeonsuuuu28 <yeonsuuuu28@gmail.com>
2023-05-13 11:28:14 +0900

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import {BrowserRouter, Routes, Route, Switch} from "react-router-dom";
+
+import Main from "./Pages/Main"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/main" element={<Main/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
